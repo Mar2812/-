22179154c3d3259cda40aeffeb98e174343f7feb
Mar2812 <75710285+Mar2812@users.noreply.github.com>
2024-09-14 13:52:03 +0800

    完成

--- self_write_transformer.py
+++ self_write_transformer.py
@@ -56,4 +56,107 @@ class MultiHeadAttention(nn.Module):
         concat = scores.transpose(1, 2).contiguous().view(batch_size, -1, self.d_model)
         output = self.out(concat)
         return output
-        
+
+class FeedForward(nn.Module):
+    # d_ff 中间层的维度
+    def __init__(self, d_model, d_ff=2048, dropout=0.1):
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+    def forward(self, x):
+        x = self.dropout(F.relu(self.linear_1(x)))
+        x = self.linear_2(x)
+        return x
+
+class NormLayer(nn.Module):
+    def __init__(self, d_model, eps=1e-6):
+        super.__init__()
+        self.d_model = d_model
+        # 可学习
+        self.alpha = nn.Parameter(torch.ones(d_model))
+        self.bias = nn.Parameter(torch.zeros(d_model))
+        self.eps = eps
+    def forward(self, x):
+        norm = self.alpha*((x-x.mean(dim=-1, keepdim=True)) / x.std(dim=-1,keepdim=True)+self.eps) + self.bias
+        return norm
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, heads, dropout=0.1) -> None:
+        super().__init__()
+        self.norm_1 = NormLayer(d_model)
+        self.norm_2 = NormLayer(d_model)
+        self.dropout_1 = nn.Dropout(dropout)
+        self.dropout_2 = nn.Dropout(dropout)
+        self.att = MultiHeadAttention(heads=heads, d_model=d_model)
+        self.ffn = FeedForward(d_model=d_model)
+    def forward(self, x, mask):
+        x_nrom = self.norm_1(x)
+        x = self.dropout_1(self.att(x_nrom, x_nrom, x_nrom, mask))
+        x_norm = self.norm_2(x)
+        x = self.dropout_2(self.ffn(x_norm))
+        return x
+
+class Encoder(nn.Module):
+    def __init__(self, vocab_size, d_model, heads, num_layers, dropout) -> None:
+        super().__init__()
+        self.pe = PositionEncoder(d_model)
+        self.embeded = nn.Embedding(vocab_size, d_model)
+        self.norm = NormLayer(d_model)
+        self.layers = nn.ModuleList([EncoderLayer(d_model=d_model, heads=heads, dropout=dropout) for _ in range(num_layers)])
+    def forward(self, src, mask):
+        x = self.embeded(src)
+        x = self.pe(x)
+        for layer in self.layers:
+            x = layer(x, mask)
+        x = self.norm(x)
+        return x
+    
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, heads, dropout=0.1) -> None:
+        super().__init__()
+        self.norm_1 = NormLayer(d_model)
+        self.norm_2 = NormLayer(d_model)
+        self.norm_3 = NormLayer(d_model)
+        self.dropout_1 = nn.Dropout(dropout)
+        self.dropout_2 = nn.Dropout(dropout)
+        self.dropout_3 = nn.Dropout(dropout)
+        self.att_1 = MultiHeadAttention(heads=heads, d_model=d_model, dropout=dropout)
+        self.att_2 = MultiHeadAttention(heads=heads, d_model=d_model, dropout=dropout)
+        self.fnn = FeedForward(d_model=d_model)
+    def forward(self, x, encoder_out, src_mask, trg_mask):
+        norm_x = self.norm_1(x)
+        x = norm_x + self.dropout_1(self.att_1(norm_x, norm_x, norm_x, trg_mask))
+        norm_x = self.norm2(x)
+        x = norm_x + self.dropout_2(self.att_2(norm_x, encoder_out, encoder_out, src_mask))
+        norm_x = self.norm_3(x)
+        x = norm_x + self.fnn(norm_x)
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, vocab_size, d_model, heads, num_layers, dropout) -> None:
+        super().__init__()
+        self.embed = nn.Embedding(vocab_size, d_model)
+        self.pe = PositionEncoder(d_model)
+        self.layers = nn.ModuleList([DecoderLayer(d_model, heads, dropout=dropout) for _ in range(num_layers)])
+        self.norm = NormLayer(d_model)
+    def formward(self, trg, encoder_out, src_mask, trg_mask):
+        embedding = self.embed(trg)
+        x = self.pe(embedding)
+        for layer in self.layers:
+            x = layer(x, encoder_out, src_mask, trg_mask)
+        return self.norm(x)
+    
+class Transformer(nn.Module):
+    def __init__(self, src_vocab, trg_vocab, d_model, num_layers, heads, dropout) -> None:
+        super().__init__()
+        self.encoder = Encoder(src_vocab, d_model, heads, num_layers, dropout)
+        self.decoder = Decoder(trg_vocab, d_model, heads, num_layers, dropout)
+        # y映射到若干词汇
+        self.linear = nn.Linear(d_model, trg_vocab)
+
+    def forward(self, src, trg, src_mask, trg_mask):
+        encoder_out = self.encoder(src, src_mask)
+        decoder_out = self.decoder(trg, encoder_out, src_mask, trg_mask)
+        output = self.linear(decoder_out)
+        return F.softmax(output)
